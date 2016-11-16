56539e6efcea105edefd15c38167196b33c861df
Nazar Bukovynsky <bukovyn@gmail.com>
2016-11-16 09:32:57 -0500

    Pseudocode

---
+++ Recount/Recount.java
@@ -0,0 +1,12 @@
+/*PSEUDOCODE
+
+	Constructor for Person object with parameters:
+													name, votes
+													
+	ArrayList of arbitruary size
+	
+	Loop through input and store values in ArrayList
+	
+	Method to go through ArrayList and retrieve name with most votes or if there is a tie
+	
+END*/
