7758fb299d9724d3ae59cde27a5faa1eed54fa7c
Nazar Bukovynsky <bukovyn@gmail.com>
2016-11-18 16:32:40 -0500

    Update Recount.java

--- Recount/Recount.java
+++ Recount/Recount.java
@@ -10,3 +10,38 @@
 	Method to go through ArrayList and retrieve name with most votes or if there is a tie
 	
 END*/
+
+//main
+import java.util.*;
+
+public class Recount{
+	public static void main(String[] args){
+		Scanner scanner = new Scanner(System.in);
+		ArrayList<String> people = new ArrayList<String>();
+		
+		while(scanner.hasNext()){
+		 people.add(scanner.nextLine());
+		}
+	    
+	}
+	
+}
+
+public boolean checkList(ArrayList<String> list)
+{
+    for(int i = 0; i < list.length; i++)
+    {
+        String temp = list.indexOf(i);
+        for(int j = 1; j < list.length; j++)
+        {
+            String tempTwo = list.indexOf(j);
+            if(temp.equals(tempTwo))
+            {
+                return false;
+                break;
+            }
+        }
+    }
+    
+    return true;
+}
